3b8db218819f4d1aa123427b87fc215c41cb6f0e
Edoardo Manenti <eoni@gft.com>
2024-12-04 21:34:51 +0100

    Day 3 incomplete solution

---
+++ day_04/test.py
@@ -0,0 +1,21 @@
+import unittest
+from aoc import part1, part2
+
+class AocTestClass(unittest.TestCase):
+    def test_part1(self):
+        test_input = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+        value = part1(test_input)
+        expected_value = 18
+        self.assertEqual(value, expected_value)
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
