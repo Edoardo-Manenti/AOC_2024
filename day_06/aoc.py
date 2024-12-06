023bd8b7cb0211be3083cc47cd89f22e1f52177a
Edoardo Manenti <eoni@gft.com>
2024-12-06 13:01:17 +0100

    Added performance measurements

--- day_06/aoc.py
+++ day_06/aoc.py
@@ -1,4 +1,7 @@
-import re
+import timeit
+
+def create_room(input_file):
+    return [list(i) for i in input_file.split("\n") if len(list(i)) != 0]
 
 def find(element, matrix):
     for i, matrix_i in enumerate(matrix):
@@ -8,29 +11,22 @@ def find(element, matrix):
     return None
 
 def part1(input_file):
-    room = [list(i) for i in input_file.split("\n") if len(list(i)) != 0]
-    #print(room)
-
+    room = create_room(input_file)
     i,j = find('^', room)
     solution = set([(i,j)])
     room[i][j] = '.'
-    #print(i, j)
     directions = [[-1,0], [0, 1], [1,0], [0, -1]]
     curr_dir = directions[0]
     while(i+curr_dir[0]<len(room) and j+curr_dir[1]<len(room[0])):
         if room[i+curr_dir[0]][j+curr_dir[1]]=='.':
             i += curr_dir[0]
             j += curr_dir[1]
-            #print("Cammino su coord: {} {}".format(i,j))
         else:
-            #print("Cambio direzione a causa di un ostacolo a coordinate: {} {}".format(i+curr_dir[0],j+curr_dir[1]))
             next_dir_idx = (directions.index(curr_dir)+1)%len(directions)
             curr_dir = directions[next_dir_idx]
             i += curr_dir[0]
             j += curr_dir[1]
         solution.add((i,j))
-        #print("Solution: {}".format(solution))
-        
     return solution
 
 def is_loop(room):
@@ -55,42 +51,47 @@ def is_loop(room):
     return False
 
 def part2(input_file):
-    room = [list(i) for i in input_file.split("\n") if len(list(i)) != 0]
+    room = create_room(input_file)
     solution = 0
     for i in range(len(room)):
         for j in range(len(room[0])):
-            #print("elemento analizzato : " + room[i][j])
             if room[i][j] not in ['#', '^']:
                 room[i][j] = '#'
                 if is_loop(room): 
                     solution += 1
-                    #print("{} {}".format(i,j))
                 room[i][j] = '.'
-
     return solution
 
 def part2_but_faster(input_file):
-    room = [list(i) for i in input_file.split("\n") if len(list(i)) != 0]
+    room = create_room(input_file)
     solution = 0
     beaten_path = part1(input_file)
     for p in beaten_path:
         i = p[0]
         j = p[1]
-        #print("elemento analizzato : " + room[i][j])
         if room[i][j] not in ['#', '^']:
             room[i][j] = '#'
             if is_loop(room): 
                 solution += 1
-                #print("{} {}".format(i,j))
             room[i][j] = '.'
 
     return solution
 
 def solution():
     input_file = open("input.txt", "r").read()
-    print(len(part1(input_file)))
+    #print(len(part1(input_file)))
     #print(part2(input_file))
-    print(part2_but_faster(input_file))
+    #print(part2_but_faster(input_file))
     
+def performance():
+    my_setup = '''
+from aoc import part1, part2, part2_but_faster
+input_file = open("input.txt", "r").read()
+'''
+    print("Time for part1: {} sec".format(timeit.timeit(setup=my_setup, stmt='part1(input_file)', number = 1)))
+    print("Time for part2: {} sec".format(timeit.timeit(setup=my_setup, stmt='part2(input_file)', number = 1)))
+    print("Time for part2_bit_faster: {} sec".format(timeit.timeit(setup=my_setup, stmt='part2_but_faster(input_file)', number = 1)))
+
 if __name__ == "__main__":
     solution()
+    #performance()
