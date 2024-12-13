600600347c5a04896b2a37ec54b1245f1a647773
Edoardo M <edoardomanenti99@gmail.com>
2024-12-13 09:16:42 +0100

    Part 2 Completed

--- day_13/aoc.py
+++ day_13/aoc.py
@@ -23,6 +23,25 @@ def solve(p, solutions):
     solutions.append(cost)
     return cost
 
+def solve2(p, solutions):
+    (a1, a2, b1, b2, p1, p2) = (int(p[0]), int(p[1]), int(p[2]), int(p[3]), int(p[4]), int(p[5]))
+    p1 = 10000000000000 + p1
+    p2 = 10000000000000 + p2
+    A = (p2*b1 - b2*p1)/(a2*b1-a1*b2)
+    B = (p1-a1*A)/b1
+    A2 = int(A)
+    B2 = int(B)
+
+    if abs(A2-A)>0.01:
+        return
+    if abs(B2-B)>0.01:
+        return
+    #if A2 + B2 > 100:
+    #    return
+    cost = 3*A + B
+    solutions.append(cost)
+    return cost
+
 def part1(input_file):
     problems = parse_input(input_file)
     print(problems)
@@ -32,7 +51,12 @@ def part1(input_file):
     return sum(solutions)
 
 def part2(input_file):
-    return 0
+    problems = parse_input(input_file)
+    print(problems)
+    solutions = []
+    for p in problems:
+        solve2(p, solutions)
+    return sum(solutions)
 
 def solution():
     input_file = open("input.txt", "r").read()
--- day_13/test.py
+++ day_13/test.py
@@ -20,7 +20,6 @@ Button B: X+27, Y+71
 Prize: X=18641, Y=10279
 """
         value = parse_input(test_input)
-        print(value)
 
     def test_solve(self):
         test_input = (94, 34, 22, 67, 8400, 5400)
@@ -53,5 +52,26 @@ Prize: X=18641, Y=10279
         self.assertEqual(value, exp_value)
 
         
+    def test_part2(self):
+        test_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+        value = part2(test_input)
+        exp_value = 0
+
+        self.assertEqual(value, exp_value)
 if __name__ == "__main__":
     unittest.main(verbosity=3)
