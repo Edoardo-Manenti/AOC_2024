a0d96ed3bbe4cd608b17c62f2d8b9e1443e12d03
Edoardo M <edoardomanenti99@gmail.com>
2024-12-13 09:09:05 +0100

    Day 13 Completed part 1

---
+++ day_13/aoc.py
@@ -0,0 +1,52 @@
+import timeit
+import math
+import re
+from collections import defaultdict
+
+def parse_input(input_file):
+    regex = r"Button A: X\+(\d*), Y\+(\d*)\nButton B: X\+(\d*), Y\+(\d*)\nPrize: X=(\d*), Y=(\d*)\n*"
+    values = re.findall(regex, input_file)
+    return values
+
+def solve(p, solutions):
+    (a1, a2, b1, b2, p1, p2) = (int(p[0]), int(p[1]), int(p[2]), int(p[3]), int(p[4]), int(p[5]))
+    A = (p2*b1 - b2*p1)/(a2*b1-a1*b2)
+    B = (p1-a1*A)/b1
+    A2 = int(A)
+    B2 = int(B)
+
+    if abs(A2-A)>0.01:
+        return
+    if abs(B2-B)>0.01:
+        return
+    cost = 3*A + B
+    solutions.append(cost)
+    return cost
+
+def part1(input_file):
+    problems = parse_input(input_file)
+    print(problems)
+    solutions = []
+    for p in problems:
+        solve(p, solutions)
+    return sum(solutions)
+
+def part2(input_file):
+    return 0
+
+def solution():
+    input_file = open("input.txt", "r").read()
+    print(part1(input_file))
+    print(part2(input_file))
+    
+def performance():
+    my_setup = '''
+from aoc import part1, part2
+input_file = open("input.txt", "r").read()
+'''
+    print("Time for part1: {} sec".format(timeit.timeit(setup=my_setup, stmt='part1(input_file)', number = 1)))
+    print("Time for part2: {} sec".format(timeit.timeit(setup=my_setup, stmt='part2(input_file)', number = 1)))
+
+if __name__ == "__main__":
+    solution()
+    #performance()
---
+++ day_13/test.py
@@ -0,0 +1,57 @@
+import unittest
+from aoc import part1, part2, parse_input, solve
+
+class AocTestClass(unittest.TestCase):
+    def test_parse_input(self):
+        test_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+        value = parse_input(test_input)
+        print(value)
+
+    def test_solve(self):
+        test_input = (94, 34, 22, 67, 8400, 5400)
+
+        value = solve(test_input, [])
+        exp_value = 280
+
+        self.assertEqual(value, exp_value)
+
+    def test_part1(self):
+        test_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279
+"""
+        value = part1(test_input)
+        exp_value = 480
+
+        self.assertEqual(value, exp_value)
+
+        
+if __name__ == "__main__":
+    unittest.main(verbosity=3)
